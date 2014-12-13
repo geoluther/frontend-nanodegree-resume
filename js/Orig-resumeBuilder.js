47649feb8b964945a5e4cd486a97a5c754da845d
geoluther <george.figgs@gmail.com>
2014-12-13 15:00:13 -0700

    first check in with my changes

---
+++ js/Orig-resumeBuilder.js
@@ -0,0 +1,122 @@
+
+// $("#main").append('George<br>');
+
+// awesomeThoughts = "I am George and I am awesome!";
+
+// var funThoughts = awesomeThoughts.replace("awesome", "fun");
+
+// $("#main").append(funThoughts);
+
+
+var bio = {
+	"myname" : "George Figgs", "role": "Web Developer", 
+	"contacts": {
+		"mobile" : "303 123-4567", 
+		"email": "me@udacity.com", 
+		"github": "geo-luther",
+		"location" : "Boulder, CO" } , 
+	"bioPic" : "http://placekitten.com/g/200/200", 
+	"welcomeMessage" : "Hello There, this is my welcome message", 
+	"skills": ["html", "css", "javascript", "python", "jquery"]
+};
+
+var education = {
+	"schools" : [
+	{ 
+		"name" : "University of Arizona", 
+		"city" : "Tucson, AZ",
+		"degree" : "BA",
+		"major" : ["Linguistics", "Biology",
+		"graduation-year" : "1997" ]
+	},
+
+	{ 
+		"name": "University of Colorado",
+		"city" : "Boulder, CO",
+		"degree" : "PhD",
+		"major" : ["Linguistics", "Cognitive Science",
+		"graduation-year" : "1997" ]
+
+	}
+	] 
+};
+
+var formattedName = HTMLheaderName.replace("%data%", bio.myname);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var formattedContact = HTMLcontactGeneric.replace("%data%", bio.contacts.mobile);
+var formattedPicture = HTMLbioPic.replace("%data%", bio.bioPic);
+var formattedWelcomeMessage = HTMLWelcomeMsg.replace("%data%", bio.welcomeMessage);
+var formattedSkills = HTMLskills.replace("%data%", bio.skills);
+
+
+$("#header").append(formattedContact);
+$("#header").append(formattedPicture);
+$("#header").append(formattedWelcomeMessage);
+$("#header").append(HTMLskillsStart);
+$("#header").append(formattedSkills);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+var work = {
+};
+
+work.position = "Digital Content Manager";
+work.city = "Boulder";
+work.yeard = "2000-2003";
+
+education["school"] = "University of Arizona";
+education["major"] = "Linguistics";
+
+var formattedMajor = HTMLschoolDegree.replace("%data%", education["major"]);
+var formattedSchool = HTMLschoolName.replace("%data%", education["school"]);
+
+$("#main").append(work.city);
+$("#main").append(work.position);
+$("#main").append(education["school"]);
+$("#main").append(education["major"]);
+
+
+var s = "audacity";
+
+var udacityizer = function(s) {
+	
+	s = s[1].toUpperCase() + s.slice(2); 
+    // Right now, the variable s === "audacity"
+    // Manipulate s to make it equal to "Udacity"
+    // Your code goes here!
+
+    return s;
+};
+
+// Did your code work? The line below will tell you!
+console.log(udacityizer(s));
+
+
+function incrementLastArrayElement(_array)  {
+    var newArray = [];
+    newArray = _array.slice(0);
+    var lastNumber = newArray.pop();
+    newArray.push(lastNumber + 1);
+    return newArray;
+}
+
+var name = "AlbERt EINstEiN";
+
+function nameChanger(oldName) {
+    var finalName = oldName;
+    // Your code goes here!
+    var firstLast = finalName.split(" ");
+    var first = firstLast[0].toLowerCase();
+    var last = firstLast[1].toUpperCase();
+
+    first = first.slice(0,1).toUpperCase() + first.slice(1);
+
+    finalName = first + " " + last;
+    
+    // Don't delete this line!
+    return finalName;
+};
+
+// Did your code work? The line below will tell you!
+console.log(nameChanger(name));
+
---
+++ js/resumeBuilder.js
@@ -0,0 +1,150 @@
+
+
+/*
+bio contains a name, role, welcomeMessage, contacts object and skills array. 
+The contacts object should contain (but doesn't have to) a mobile number, 
+email address, github username, twitter handle and location.
+*/
+var bio = {
+
+	"name" : "George Figgs", 
+	"role": "Web Developer", 
+	"contacts": {
+		"mobile" : "303 123-4567", 
+		"email": "me@udacity.com", 
+		"github": "geo-luther",
+		"location" : "Boulder, CO" } , 
+	"bioPic" : "http://placekitten.com/g/200/200", 
+	"welcomeMessage" : "Hello There, this is my welcome message", 
+	"skills": ["html", "css", "javascript", "python", "jquery"]
+	};
+
+/*
+education contains an array of schools. Each school object in schools contains a name, 
+location, degree, majors array, dates attended and a url for the school's website. 
+education also contains an onlineCourses array. 
+Each onlineCourse object in onlineCourses should contain a title, 
+school, dates attended and a url for the course.
+*/
+
+var education = {
+	"schools" : [
+	{ 
+		"name" : "University of Arizona", 
+		"location" : "Tucson, AZ",
+		"degree" : "BA, BS",
+		"majors" : ["Linguistics", "Biology"],
+		"dates" : "1989-1994",
+		"url" : "http://www.Arizona.edu"
+	},
+
+	{ 
+		"name" : "University of Colorado", 
+		"location" : "Boulder, CO",
+		"degree" : "MA, ABD",
+		"majors" : ["Linguistics", "Cognitive Science"],
+		"dates" : "1997-2007",
+		"url" : "www.colorado.edu"
+	} ],
+
+	"onlineCourses" : [
+	{ 
+		"title" : "Programming in Java",
+		"dates" : "Udacity",
+		"url" : "Yes"
+	},
+
+	{
+		"title" : "Programming in Java",
+		"school" : "Udacity",
+		"dates" : "2013",
+		"url" : "www.udacity.com"
+	} ]
+};
+
+
+// Work contains an array of jobs. 
+// Each job object in jobs should contain an employer, title, location, dates worked and description
+
+var work = {
+	"jobs" : [
+	{
+		"employer" : "KGNU",
+		"title" : "Digital Content Manager",
+		"location" : "Boulder, CO",
+		"dates" : "2013-Present",
+		"description" : "Editor, Producer, Web Developer"
+	},
+
+	{
+		"employer" : "JD Power and Associates",
+		"title" : "Science Annotator",
+		"location" : "Boulder, CO",
+		"dates" : "2012",
+		"description" : "Semantic and Syntactic Annotation of Language Corpora"
+	},
+
+	{
+		"employer" : "The Big Cheese",
+		"title" : "Cheese Monger",
+		"location" : "Boulder, CO",
+		"dates" : "2014",
+		"description" : "eat, drink and be merry, for me may run out of dairy"
+	}
+	]
+};
+
+
+// projects contains an array of projects. 
+// Each project object in projects should contain a title, 
+// dates worked, description, and 
+// an images array with URL strings for project images.
+var projects = {
+
+	"projects" : [
+
+	{
+		"title" : "news.kgnu.org",
+		"dates" : "2014",
+		"description" : "Radio News Magazine Website that publishes text, images and audio from daily news programming",
+		"images" : ["http://www.kgnu.org//index.htm-old/index.fall2014.htm"]
+	},
+
+	{
+		"title" : "",
+		"dates" : "",
+		"description" : "",
+		"images" : ["", ""]
+	} ]
+};
+
+
+if (bio.skills.length > 0 ) {
+	$("#header").append(HTMLskillsStart);
+
+	for (skill in bio.skills) {
+		var formattedSkill = HTMLskills.replace("%data%", bio.skills[skill]);
+		$("#skills").append(formattedSkill);
+	}
+
+}
+
+
+for (job in work.jobs) {
+
+	$("#workExperience").append(HTMLworkStart);
+
+	var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+	var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+	// location, dates, description
+	$(".work-entry:last").append(formattedEmployer + formattedTitle);
+
+	var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+	var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+	var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+
+	$(".work-entry:last").append(formattedDates);
+	$(".work-entry:last").append(formattedLocation);
+	$(".work-entry:last").append(formattedDescription);
+
+}
